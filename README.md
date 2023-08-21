c970d20df135d16296398c044d479eaa09486956
Mamadou MARÉGA <hamshakour93@gmail.com>
2023-08-21 16:25:25 +0200

    Added Readme file and installation process file

---
+++ README.md
@@ -0,0 +1,11 @@
+# Clonez le projet 
+
+Remplacer les addresses ip du fichier inventory par les vôtres
+Faites la même choses dans les playbooks.
+
+Et à la fin lancez le fichier install.sh 
+``./install.sh``
+
+Changez le /etc/hosts de votre machine de test comme 
+par exemple ici ``IP_DE_S0 devops.com devsec.com devsecops.com
+``
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Début configuration réseau"
+ansible-playbook -i inventory.ini playbook/dhcp_dns/install_dnsmasq.yml
+ansible-playbook -i inventory.ini playbook/dhcp_dns/configure_resolv_conf.yml
+ansible-playbook -i inventory.ini playbook/dhcp_dns/configure_dhcp.yml
+ansible-playbook -i inventory.ini playbook/dhcp_dns/configure_dns.yml
+echo "Fin configuration réseau"
+
+echo "Début installations des services"
+ansible-playbook -i inventory.ini playbook/apache/install_apache.yml
+ansible-playbook -i inventory.ini playbook/haproxy/install_haproxy.yml
+ansible-playbook -i inventory.ini playbook/mariadb/install_mariadb.yml
+ansible-playbook -i inventory.ini playbook/nfs/install_nfs.yml
+echo "Fin installations des services"
+
+echo "Installation des wordpress"
+ansible-playbook -i inventory.ini playbook/wordpress/wordpress.yml
+echo "Fin installation des wordpress"
